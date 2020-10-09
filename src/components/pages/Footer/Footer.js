4fb4aa9658a52d6a11b0d29fccc816cb5645a083
Ardie Wijaya <solience2013@icloud.com>
2020-10-09 16:59:36 +0700

    initial commit

---
+++ src/components/pages/Footer/Footer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from '../../Button';
+import { Link } from 'react-router-dom';
+import {
+    FaFacebook,
+    FaInstagram,
+    FaYoutube,
+    FaTwitter,
+    FaLinkedin,
+    FaWhatsapp,
+    FaBrain
+} from 'react-icons/fa';
+import { MdFingerprint } from 'react-icons/md';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className='social-media'>
+                <div className='social-media-wrap'>
+                    <div className='footer-logo'>
+                        <Link to='/' className='social-logo'>
+                            <FaBrain className='navbar-icon' />
+            </Link>
+                    </div>
+                    <small className='website-rights'>Ardie Wijaya Eka Putra © 2020</small>
+                    <div className='social-icons'>
+                        <Link
+                            className='social-icon-link'
+                            to='/'
+                            target='_blank'
+                            aria-label='Whatsapp'
+                        >
+                            <FaWhatsapp />
+                        </Link>
+                        <Link
+                            className='social-icon-link'
+                            to='/'
+                            target='_blank'
+                            aria-label='Instagram'
+                        >
+                            <FaInstagram />
+                        </Link>
+                        <Link
+                            className='social-icon-link'
+                            to='/'
+                            target='_blank'
+                            aria-label='Twitter'
+                        >
+                            <FaTwitter />
+                        </Link>
+                        <Link
+                            className='social-icon-link'
+                            to='/'
+                            target='_blank'
+                            aria-label='LinkedIn'
+                        >
+                            <FaLinkedin />
+                        </Link>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Footer;
